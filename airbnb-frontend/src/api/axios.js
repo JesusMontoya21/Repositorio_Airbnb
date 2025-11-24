c71070f61c308007dc2ba6ef60627c7f21e5f2a0
lf.ramos21 <lf.ramos21@info.uas.edu.mx>
2025-11-24 08:23:36 -0700

    Reestructuracion de Proyecto (Docker) (REQ204)

--- airbnb-frontend/src/api/axios.js
+++ airbnb-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'http://localhost',
   withCredentials: true,
   xsrfCookieName: 'XSRF-TOKEN',
   xsrfHeaderName: 'X-XSRF-TOKEN',
